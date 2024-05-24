0d7bcaeb6bf83198ff381af466ee14436a5a09d0
Dazegambler <70617250+Dazegambler@users.noreply.github.com>
2024-05-24 18:08:52 -0300

    Update TestLib.c

--- th155r/Testlib/TestLib.c
+++ th155r/Testlib/TestLib.c
@@ -1,5 +1,18 @@
-#include <stdio.h>
+#include <windows.h>
 
-void say_hello(){
-    printf("Hello, from TestLib!\n");
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+        case DLL_PROCESS_ATTACH:
+            MessageBox(NULL,"DLL Injected!","Success", MB_OK);
+            break;
+        case DLL_THREAD_ATTACH:
+        case DLL_THREAD_DETACH:
+        case DLL_PROCESS_DETACH:
+            break;
+    }
+    return TRUE;
+}
+
+__declspec(dllexport) void HelloWorld() {
+    MessageBox(NULL,"Hello, World!","Message", MB_OK);
 }

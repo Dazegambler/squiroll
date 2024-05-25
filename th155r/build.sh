457632b8a8f0be151c6c4522b084ce2e30e52799
Dazegambler <70617250+Dazegambler@users.noreply.github.com>
2024-05-25 19:17:11 -0300

    Update build.sh

--- th155r/build.sh
+++ th155r/build.sh
@@ -1,2 +1,3 @@
-x86_64-w64-mingw32-gcc -o th155r.exe main.c
-x86_64-w64-mingw32-gcc -shared -o Netcode.dll Testlib/TestLib.c 
+i686-w64-mingw32-gcc -o th155r.exe main.c 
+#i686-w64-mingw32-gcc -shared -o Netcode.dll Testlib/TestLib.c -Wl,--exclude-all-symbols
+i686-w64-mingw32-gcc -Wl,--exclude-all-symbols,--subsystem,windows,--kill-at -shared -o Netcode.dll Testlib/TestLib.c

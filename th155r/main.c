193fbcf825e8dbeb8d13ffa2d50a7f2c13832530
Dazegambler <70617250+Dazegambler@users.noreply.github.com>
2024-05-25 17:22:31 -0300

    Update main.c

--- th155r/main.c
+++ th155r/main.c
@@ -3,275 +3,145 @@
 #include <tlhelp32.h>
 #include <tchar.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
 
 #define TH155 "th155.exe"
 #define NETCODE "Netcode.dll"
 //#define FUNC "say_hello" // Placeholder
 
-char* GetLastErrorMessage() {
-    DWORD error_code = GetLastError();
-    if (error_code == 0) {
-        char* buffer = (char*)LocalAlloc(LMEM_FIXED, sizeof("No error."));
-        if (buffer) {
-            strcpy(buffer, "No error.");
-        }
-        return buffer;
-    }
 
-    char* buffer = NULL;
-    DWORD message_length = FormatMessage(
-        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-        NULL,
-        error_code,
-        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-        (LPSTR)&buffer,
-        0,
-        NULL
-    );
+//Code provided by zero318
+static uint8_t inject_func[] = {
+	0x53,             //   PUSH EBX
+	0x6A, 0x00,       //   PUSH 0
+	0x6A, 0x00,       //   PUSH 0
+	0x68, 0, 0, 0, 0, //   PUSH dll_name
+	0xE8, 0, 0, 0, 0, //   CALL LoadLibraryExW
+	0x85, 0xC0,       //   TEST EAX, EAX
+	0x74, 0x30,       //   JZ load_library_fail
+	0x89, 0xC3,       //   MOV EBX, EAX
+	0x68, 0, 0, 0, 0, //   PUSH init_func_name
+	0x50,             //   PUSH EAX
+	0xE8, 0, 0, 0, 0, //   CALL GetProcAddress
+	0x85, 0xC0,       //   TEST EAX, EAX
+	0x74, 0x32,       //   JZ get_init_func_fail
+	0x68, 0, 0, 0, 0, //   PUSH init_func_param
+	0xFF, 0xD0,       //   CALL EAX
+	0x89, 0xD9,       //   MOV ECX, EBX
+	0x89, 0xD9,       //   MOV EBX, EAX
+	0x85, 0xC0,       //   TEST EAX, EAX
+	0x74, 0x09,       //   JZ init_func_success
+	0x83, 0xCB, 0xFF, //   OR EBX, -1
+					  // free_library:
+	0x51,             //   PUSH ECX
+	0xE8, 0, 0, 0, 0, //   CALL FreeLibrary
+					  // exit_thread:
+	0x53,             //   PUSH EBX
+	0xE8, 0, 0, 0, 0, //   CALL ExitThread
+	0xCC,             //   INT3
+					  // load_library_fail:
+	0x31, 0xDB,       //   XOR EBX, EBX
+					  //   CMP DWORD PTR FS:[LastErrorValue], STATUS_DLL_INIT_FAILED
+	0x64, 0x81, 0x3D, 0x34, 0x00, 0x00, 0x00, 0x42, 0x01, 0x00, 0xC0,
+	0x0F, 0x95, 0xC3, //   SETNE BL
+	0x43,             //   INC EBX
+	0xEB, 0xE6,       //   JMP exit_thread
+					  // get_init_func_fail:
+	0x89, 0xD9,       //   MOV ECX, EBX
+					  //   MOV EBX, 3
+	0xBB, 0x03, 0x00, 0x00, 0x00,
+	0xEB, 0xD7,       //   JMP free_library
+	0xCC
+};
 
-    if (message_length == 0) {
-        buffer = (char*)LocalAlloc(LMEM_FIXED, 64);
-        if (buffer) {
-            snprintf(buffer, 64, "Unknown error code: %lu", error_code);
-        }
-    }
+const size_t inject_func_length = sizeof(inject_func);
+const size_t dll_name = 6;
+const size_t load_library = 11;
+const size_t init_func_name = 22;
+const size_t get_proc_address = 28;
+const size_t init_func_param = 37;
+const size_t free_library = 56;
+const size_t exit_thread = 62;
 
-    return buffer;
+typedef enum {
+	INJECT_INIT_FALSE = -1, // Initialization function returned false
+	INJECT_SUCCESS = 0,
+	INJECT_DLL_FALSE = 1, // DllMain returned FALSE
+	INJECT_DLL_ERROR = 2, // LoadLibrary failed
+	INJECT_INIT_ERROR = 3, // Initialization function not found,
+	INJECT_ALLOC_FAIL = 4, // One of the virtual memory functions failed
+	INJECT_THREAD_FAIL = 5, // CreateRemoteThread failed
+} InjectReturnCode;
+
+InjectReturnCode inject(HANDLE process, const wchar_t* dll_str, const char* init_func, int32_t param) {
+	size_t dll_name_bytes = (wcslen(dll_str) + 1) * sizeof(wchar_t);
+	size_t init_func_bytes = strlen(init_func) + 1;
+
+	uint8_t* inject_buffer = (uint8_t*)VirtualAllocEx(process, NULL, inject_func_length + dll_name_bytes + init_func_bytes, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+	if (!inject_buffer) {
+		return INJECT_ALLOC_FAIL;
+	}
+
+    // Don't question this part
+	*(uint8_t**)&inject_func[dll_name] = &inject_buffer[inject_func_length];
+	*(int32_t*)&inject_func[load_library] = (uint8_t*)&LoadLibraryExW - &inject_buffer[load_library + sizeof(int32_t)];
+	*(uint8_t**)&inject_func[init_func_name] = &inject_buffer[inject_func_length + dll_name_bytes];
+	*(int32_t*)&inject_func[get_proc_address] = (uint8_t*)&GetProcAddress - &inject_buffer[get_proc_address + sizeof(int32_t)];
+	*(int32_t*)&inject_func[init_func_param] = param;
+	*(int32_t*)&inject_func[free_library] = (uint8_t*)&FreeLibrary - &inject_buffer[free_library + sizeof(int32_t)];
+	*(int32_t*)&inject_func[exit_thread] = (uint8_t*)&ExitThread - &inject_buffer[exit_thread + sizeof(int32_t)];
+
+	DWORD exit_code = INJECT_ALLOC_FAIL;
+	if (
+		WriteProcessMemory(process, inject_buffer, inject_func, inject_func_length, NULL) &&
+		WriteProcessMemory(process, inject_buffer + inject_func_length, dll_str, dll_name_bytes, NULL) &&
+		WriteProcessMemory(process, inject_buffer + inject_func_length + dll_name_bytes, init_func, init_func_bytes, NULL) &&
+		FlushInstructionCache(process, inject_buffer, inject_func_length)
+	) {
+		exit_code = INJECT_THREAD_FAIL;
+		HANDLE thread = CreateRemoteThread(process, NULL, 0, (LPTHREAD_START_ROUTINE)inject_buffer, 0, 0, NULL);
+		if (thread) {
+			WaitForSingleObject(thread, INFINITE);
+			GetExitCodeThread(thread, &exit_code);
+			CloseHandle(thread);
+		}
+	}
+
+	VirtualFreeEx(process, inject_buffer, 0, MEM_RELEASE);
+
+	return (InjectReturnCode)exit_code;
 }
 
-
-BOOL InjectDLL(HANDLE hProcess, const char *dllPath, HMODULE *outhModule)
+BOOL execute_program_inject()
 {
     FILE *log = fopen("th155r.log", "a");
-    if (hProcess == 0)
-    {
-        fprintf(log, "No Process given...(%d)\n", GetLastError());
-        fclose(log);
-        return FALSE;
-    }
-
-    SIZE_T pathLen = strlen(dllPath);
-    LPVOID remoteString = (LPVOID)VirtualAllocEx(hProcess, NULL, sizeof(wchar_t)*pathLen+1, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
-    if (!remoteString)
-    {
-        fprintf(log, "Could not allocate memory in target process...(%d)\n", GetLastError());
-        fclose(log);
-        return FALSE;
-    }
-
-    if (!WriteProcessMemory(hProcess, remoteString,dllPath,sizeof(wchar_t)*pathLen, NULL))
-    {
-        fprintf(log, "Could not write to process memory...(%d)\n", GetLastError());
-        VirtualFreeEx(hProcess, remoteString, 0, MEM_RELEASE);
-        fclose(log);
-        return FALSE;
-    }
-
-    HMODULE hKernel32 = GetModuleHandleA("Kernel32.dll");
-    if (!hKernel32)
-    {
-        fprintf(log, "Failed to get Kernel32.dll...(%d)\n", GetLastError());
-        VirtualFreeEx(hProcess, remoteString, 0, MEM_RELEASE);
-        fclose(log);
-        return FALSE;
-    }
-
-    LPVOID lb = GetProcAddress(hKernel32, "LoadLibraryA");
-    if (!lb)
-    {
-        fprintf(log, "Failed to get LoadLibrary...(%d)\n", GetLastError());
-        VirtualFreeEx(hProcess, remoteString, 0, MEM_RELEASE);
-        fclose(log);
-        return FALSE;
-    }
-
-    // NOT_CAUSES:
-    // hprocess
-    // injected string
-    // loadlibrary address(?)
+    STARTUPINFOW si = { sizeof(STARTUPINFOW) };
+	PROCESS_INFORMATION pi = {};
     
-    fprintf(log, "Params: %p | %p...(%d)\n", hProcess, lb);
-    HANDLE hThread = CreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)lb, remoteString, 0, NULL);
-    fprintf(log, "Thread: %p\n", hThread);
-    if (!hThread)
-    {
-        fprintf(log, "Could not create remote thread...(%d)\n", GetLastError());
-        VirtualFreeEx(hProcess, remoteString, 0, MEM_RELEASE);
-        fclose(log);
-        return FALSE;
-    }
-
-    WaitForSingleObject(hThread, INFINITE);
-
-    DWORD exitCode;
-    if (!GetExitCodeThread(hThread, &exitCode))
-    {
-        fprintf(log, "Failed getting thread exit code...(%d)\n", GetLastError());
-        CloseHandle(hThread);
-        VirtualFreeEx(hProcess, remoteString, 0, MEM_RELEASE);
-        fclose(log);
-        return FALSE;
-    }
-
-    *outhModule = (HMODULE)&exitCode;
-
-    VirtualFreeEx(hProcess, remoteString, 0, MEM_RELEASE);
-    CloseHandle(hThread);
-    fclose(log);
-    return TRUE;
-}
-
-/*NOT NEEDED ANYMORE(?)*/void *get_remote_function_address(HANDLE hProcess, HMODULE hModule /*Injected Library*/, const char *functionName)
-{
-    FILE *log = fopen("th155r.log", "a");
-    void *functionAddress = GetProcAddress(hModule, functionName);
-    if (!functionAddress)
-    {
-        fprintf(log, "Could not get function address locally...(%d)\n", log);
-        fclose(log);
-        return NULL;
-    }
-    fclose(log);
-    return functionAddress;
-}
-
-/*NOT NEEDED ANYMORE(?)*/BOOL patch_memory_with_jmp(HANDLE hProcess, void *targetAddress, void *jmpAddress)
-{
-    FILE *log = fopen("th155r.log", "a");
-    BYTE jmpInstruction[5] = {0xE9, 0, 0, 0, 0}; // jmp instruction
-    DWORD offset = (DWORD)((BYTE *)jmpAddress - (BYTE *)targetAddress - 5);
-
-    memcpy(&jmpInstruction[1], &offset, 4);
-
-    if (!WriteProcessMemory(hProcess, targetAddress, jmpInstruction, sizeof(jmpInstruction), NULL))
-    {
-        fprintf(log, "Could not write JMP instruction...(%d)\n", GetLastError());
-        fclose(log);
-        return FALSE;
-    }
-    fclose(log);
-    return TRUE;
-}
-
-BOOL execute_program(const char *program_path)
-{
-    FILE *log = fopen("th155r.log", "a");
-    STARTUPINFO si;
-    PROCESS_INFORMATION pi;
-
-    ZeroMemory(&si, sizeof(si));
-    si.cb = sizeof(si);
-    ZeroMemory(&pi, sizeof(pi));
-
-    if (!CreateProcess(NULL, (LPSTR)program_path, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
-    {
-        fprintf(log, "CreateProcess failed...(%d)\n", log);
-        return FALSE;
-    }
-
-    fclose(log);
-    CloseHandle(pi.hThread);
-    CloseHandle(pi.hProcess);
-    return TRUE;
-}
-
-HANDLE findProcess(const char *processName)
-{
-    FILE *log = fopen("th155r.log", "a");
-    HANDLE hProcessSnap;
-    HANDLE hProcess;
-
-    hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-    if (hProcessSnap == INVALID_HANDLE_VALUE)
-    {
-        fprintf(log, "Could not create snapshot...(%d)\n", GetLastError());
-        fclose(log);
-        return NULL;
-    }
-
-    PROCESSENTRY32 pe32;
-    pe32.dwSize = sizeof(PROCESSENTRY32);
-
-    if (!Process32First(hProcessSnap, &pe32))
-    {
-        fprintf(log, "Failed to get first process...(%d)\n", log);
-        fclose(log);
-        CloseHandle(hProcessSnap);
-        return FALSE;
-    }
-
-    do
-    {
-        if (!_tcsicmp(pe32.szExeFile, processName))
-        {
-            fprintf(log, "th155 was found in memory\n", log);
-            fprintf(log, "path:%s\n", pe32.szExeFile);
-            hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pe32.th32ProcessID);
-            if (hProcess != NULL)
-            {
-                fprintf(log, "Found you!\n");
-                fclose(log);
-                return hProcess;
-            }
-            else
-            {
-                fprintf(log, "Failed to open process...(%d)\n", log);
-                fclose(log);
-                return NULL;
-            }
+    bool ret = false;
+    if (CreateProcessW(L"th155.exe",NULL, NULL, NULL, TRUE, CREATE_SUSPENDED, NULL, NULL, &si, &pi)) {
+        InjectReturnCode inject_result = inject(pi.hProcess, L"Netcode.dll", "netcode_init", 0);
+        if (inject_result == INJECT_SUCCESS) {
+            ResumeThread(pi.hThread);
+            ret = true;
+        } else {
+            fprintf(log,"Code Injection failed...(%d)\n",GetLastError());
         }
-    } while (Process32Next(hProcessSnap, &pe32));
-    fprintf(log, "th155 has not been loaded into memory, aborting...(%d)\n", log);
+        CloseHandle(pi.hThread);
+        CloseHandle(pi.hProcess);
+    }
     fclose(log);
-    return NULL;
+    return ret;
 }
 
 int main()
 {
     FILE *log = fopen("th155r.log", "a");
     fprintf(log, "Starting patcher\n");
-    /*NOT NEEDED ANYMORE(?)*///LPVOID targetAddress = (LPVOID)0x12345678; // Placeholder address
-    if (!execute_program(TH155))
-    {
-        fclose(log);
-        return 1;
-    }
-    HANDLE hProcess = findProcess(TH155);
-    if (!hProcess)
-    {
-        fprintf(log, "Failed to start process...(%d)\n", GetLastError());
-        fclose(log);
-        return 1;
-    }
-    HMODULE hModule;
-    if (!InjectDLL(hProcess, NETCODE, &hModule))
-    {
-        fprintf(log, "Failed to inject dll...(%d)\n", GetLastError());
-        CloseHandle(hProcess);
-        fclose(log);
-        return 1;
-    }
-
-    //The library itself can do the function injections
-    // LPVOID jmpAddress = get_remote_function_address(hProcess, hModule, FUNC);
-    // if (!jmpAddress)
-    // {
-    //     fprintf(log, "Failed to get remote function address...(%d)\n", GetLastError());
-    //     fclose(log);
-    //     CloseHandle(hProcess);
-    //     return 1;
-    // }
-
-    // if (!patch_memory_with_jmp(hProcess, targetAddress, jmpAddress))
-    // {
-    //     fprintf(log, "Failed to patch memory...(%d)\n", GetLastError());
-    //     CloseHandle(hProcess);
-    //     fclose(log);
-    //     return 1;
-    // }
-    //fprintf(log, "it actually worked");
+    execute_program_inject();
     fclose(log);
-    CloseHandle(hProcess);
     return 0;
 }
 

174ab3073a758934d3c5cf338ef173a12fa8edab
Dazegambler <jadir689@gmail.com>
2024-06-16 18:18:08 -0300

    added func injection

--- th155r/Netcode/main.c
+++ th155r/Netcode/main.c
@@ -1,6 +1,14 @@
 #include <winsock2.h>
 #include <windows.h>
+#include <winuser.h>
 #include <stdint.h>
+#include <stdio.h>
+
+typedef void (*func_ptr)(void);
+
+uintptr_t base_address() {
+    return (uintptr_t)GetModuleHandleA(NULL);
+}
 
 void mem_write(LPVOID address, const BYTE* data, size_t size) {
     DWORD oldProtect;
@@ -10,8 +18,18 @@ void mem_write(LPVOID address, const BYTE* data, size_t size) {
     }
 }
 
-uintptr_t base_address() {
-    return (uintptr_t)GetModuleHandleA(NULL);
+
+void inject_func(void* addr/*RVA*/, func_ptr local_func, size_t original_size) {
+    uintptr_t local_func_address = (uintptr_t)local_func;
+    uintptr_t addr_address = (uintptr_t)addr+base_address();
+    int32_t offset = (int32_t)(local_func_address - addr_address - 5);
+
+    uint8_t jump_instruction[5] = { 0xE9 };
+    memcpy(&jump_instruction[1], &offset, 4);
+
+    unsigned char nop[] = {0x90};
+    mem_write(addr,nop, original_size);
+    mem_write(addr,jump_instruction,sizeof(jump_instruction));
 }
 
 void yes_tampering(){
@@ -27,17 +45,25 @@ void yes_tampering(){
 
 }
 
+void update_window_title(){
+    // Rx4DAD30
+}
+
+void testInjection(){
+    FILE *fp;
+    fp = fopen("TEST.txt","w");
+    fprintf(fp,"Successful test");
+    fclose(fp);
+    asm (
+        "call 0x0056fde0\n\t"
+    );
+}
+
 //FUNCTION REQUIRED FOR THE LIBRARY
 __declspec(dllexport) int __stdcall netcode_init(int32_t param) {
-
-    //Rx12E820,Rx130630,Rx132AF0
-	//0xC3
     yes_tampering();
 
-
-
-    //0x00570E28,0x00570E88,0x00570E98
-    BYTE data[] = {0xC3};
-    //mem_write((LPVOID)0x00572bc5,data,sizeof(data));
+    uintptr_t addr = 0x00170e98; 
+    inject_func((LPVOID)addr,testInjection,5);
     return 0;
 }

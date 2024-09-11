de105c4675b05395bb424f003ddc2e5c13fb3fb8
Dazegambler <jadir689@gmail.com>
2024-09-11 01:48:40 -0300

    db update

--- th155r/Netcode/plugin.cpp
+++ th155r/Netcode/plugin.cpp
@@ -9,31 +9,27 @@
 #include <squirrel.h>
 
 #include "util.h"
+#include "AllocMan.h"
 #include "log.h"
 
 extern "C" {
     dll_export int stdcall init_instance_v2(void*) {
-        log_printf("Hello from init!");
         return 1;
     }
 
     dll_export int stdcall release_instance() {
-        log_printf("Hello from release!");
         return 1;
     }
 
     dll_export int stdcall update_frame() {
-        log_printf("Hello from update!");
         return 1;
     }
 
     dll_export int stdcall render_preproc() {
-        log_printf("Hello from preproc");
         return 0;
     }
 
     dll_export int stdcall render(int, int) {
-        log_printf("Hello from render");
         return 0;
     }
 }

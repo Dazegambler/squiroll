02d3bea397784f96f91216220cf5dba794ef7c05
Khang <khang06@users.noreply.github.com>
2024-12-11 00:40:30 -0500

    Run condense_nut on Windows

--- build.bat
+++ build.bat
@@ -1,21 +1,32 @@
 @echo off
 
-set TOOLS_PATH=.\tools\make_embed_windows.exe
+set MAKE_EMBED_PATH=tools\make_embed_windows.exe
+set CONDENSE_NUT_PATH=tools\condense_nut_windows.exe
 set REPLACEMENT_FILES_DIR=replacement_files
+set REPLACEMENT_COMPRESSED_DIR=compressed_nuts\replacement_files
 set REPLACEMENT_DESTINATION_DIR=th155r\Netcode\replacement_files
 
 set NEW_FILES_DIR=new_files
+set NEW_COMPRESSED_DIR=compressed_nuts\new_files
 set NEW_DESTINATION_DIR=th155r\Netcode\new_files
 
 mkdir "%REPLACEMENT_DESTINATION_DIR%"
 mkdir "%NEW_DESTINATION_DIR%"
 
 for %%F in (%REPLACEMENT_FILES_DIR%\*) do (
-    %TOOLS_PATH% %%F %REPLACEMENT_DESTINATION_DIR%\%%~nxF.h
+    %CONDENSE_NUT_PATH% %%F %REPLACEMENT_COMPRESSED_DIR%\%%~nxF
+)
+
+for %%F in (%REPLACEMENT_COMPRESSED_DIR%\*) do (
+    %MAKE_EMBED_PATH% %%F %REPLACEMENT_DESTINATION_DIR%\%%~nxF.h
 )
 
 for %%F in (%NEW_FILES_DIR%\*) do (
-    %TOOLS_PATH% %%F %NEW_DESTINATION_DIR%\%%~nxF.h
+    %CONDENSE_NUT_PATH% %%F %NEW_COMPRESSED_DIR%\%%~nxF
+)
+
+for %%F in (%NEW_COMPRESSED_DIR%\*) do (
+    %MAKE_EMBED_PATH% %%F %NEW_DESTINATION_DIR%\%%~nxF.h
 )
 
 set DEFINES=-D_CRT_SECURE_NO_WARNINGS -D_WINSOCK_DEPRECATED_NO_WARNINGS -DNOMINMAX -D_WINSOCKAPI_ -D_CRT_SECURE_NO_DEPRECATE -D_CRT_NONSTDC_NO_DEPRECATE -D_CRT_DECLARE_NONSTDC_NAMES

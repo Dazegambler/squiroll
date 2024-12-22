ea7ade0abbff238f9c72a29ab5e0d3cd43f47d52
Khang <khang06@users.noreply.github.com>
2024-12-22 01:53:38 -0500

    Fix lobby build.bat

--- src/lobby/build.bat
+++ src/lobby/build.bat
@@ -1,2 +1,2 @@
 @echo off
-clang++ -std=c++20 -O2 aocf_server.cpp -o aocf_server.exe -Wno-switch -Wno-c99-designator -Wno-vla-cxx-extension -ferror-limit=100
+clang-cl -m32 /std:c++20 -O2 aocf_server.cpp -Wno-narrowing -DNOMINMAX -D_WINSOCKAPI_ -I../shared -o aocf_server.exe -Wno-switch -Wno-c99-designator -Wno-vla-cxx-extension -ferror-limit=100

4e520a68e59c7d741a02a4ea8120721002d5aeae
penguin <justapenguin974@gmail.com>
2024-12-22 12:19:45 -0500

    Update tools build.sh to use clang

--- tools/build.sh
+++ tools/build.sh
@@ -1,2 +1,4 @@
-g++ -std=c++20 -o make_embed_linux.run make_embed.cpp
-g++ -std=c++20 -o condense_nut_linux.run condense_nut.cpp
+#!/usr/bin/env sh
+
+clang++ -std=c++20 -o make_embed_linux.run make_embed.cpp
+clang++ -std=c++20 -o condense_nut_linux.run condense_nut.cpp

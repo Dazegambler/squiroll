2cc5a4bc0e5618c9efab98960e173f68cd4fdf79
penguin <justapenguin974@gmail.com>
2024-12-22 23:03:54 -0500

    Add back the clang 18 specifiers
    Since not all Linux distros have the latest version of clang in the
    repos, specific the exact version of clang to use instead of the default
    version.

--- src/lobby/build.sh
+++ src/lobby/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-clang++ -std=c++20 -O2 aocf_server.cpp -o aocf_server.run -I../shared -Wno-switch -Wno-c99-designator -Wno-vla-cxx-extension -Wno-narrowing -DNOMINMAX -D_WINSOCKAPI_ -ferror-limit=100
+clang++-18 -std=c++20 -O2 aocf_server.cpp -o aocf_server.run -I../shared -Wno-switch -Wno-c99-designator -Wno-vla-cxx-extension -Wno-narrowing -DNOMINMAX -D_WINSOCKAPI_ -ferror-limit=100
--- tools/build.sh
+++ tools/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-clang++ -std=c++20 -o make_embed_linux.run make_embed.cpp
-clang++ -std=c++20 -o condense_nut_linux.run condense_nut.cpp
+clang++-18 -std=c++20 -o make_embed_linux.run make_embed.cpp
+clang++-18 -std=c++20 -o condense_nut_linux.run condense_nut.cpp
